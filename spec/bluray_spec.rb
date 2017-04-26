5d2043d19b0c36b973b6b0cb9ff880bc9956cbb8
Lou Lee <louisa.leeyh@gmail.com>
2017-04-26 16:44:47 +0800

    add bluray test

--- spec/bluray_spec.rb
+++ spec/bluray_spec.rb
@@ -3,11 +3,43 @@ require_relative '../lib/bluray'
 
 describe Bluray do
   before(:context) do
-    # initialize item
+    @bluray1 = Bluray.new('Winnie the Pooh', 12.99, 90, 'Piglet', 'Tigger')
   end
 
-  # check initialization
-  # check that it is an extended from Item
-  # check that it is an instance of Bluray
-  # check getters and setters
+  describe Initialization do
+    it 'should be an instance of Bluray' do
+      expect(@bluray1).to be_instance_of Bluray
+    end
+
+    it 'should be extended from item' do
+      expect(@bluray1).to be_kind_of(Item)
+    end
+  end
+
+  describe 'check getters and setters' do
+    it 'can read and assign name' do
+     @bluray1.name = 'Batman'
+     expect(@bluray1.name).to eq('Batman')
+    end
+
+    it 'can read and assign price' do
+     @bluray1.price = '9.99'
+     expect(@bluray1.price).to eq('9.99')
+    end
+
+    it 'can read and assign runtime' do
+     @bluray1.runtime = '120'
+     expect(@bluray1.runtime).to eq('120')
+    end
+
+    it 'can read and assign director' do
+     @bluray1.director = 'Christopher Nolan'
+     expect(@bluray1.director).to eq('Christopher Nolan')
+    end
+
+    it 'can read and assign producer' do
+     @bluray1.producer = 'Emma Thomas'
+     expect(@bluray1.producer).to eq('Emma Thomas')
+    end
+  end
 end
