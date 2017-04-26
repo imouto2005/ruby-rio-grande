58b2b962d4912273a3d2b239aa6183313cc49bb6
Tom Lee <d.dorkblue@gmail.com>
2017-04-26 17:56:27 +0800

    passed test for bluray

--- lib/bluray.rb
+++ lib/bluray.rb
@@ -1,4 +1,11 @@
 require_relative 'item.rb'
 
 class Bluray < Item
+  attr_accessor :name, :price, :runtime, :director, :producer
+  def initialize(name, price, runtime, director, producer)
+    super(name, price)
+    @runtime = runtime
+    @director = director
+    @producer = producer
+  end
 end
